29b834e523d1e1f582480397ab7e0123c9ff8549
Bill Wheeler <admin@phpdreams.com>
2020-07-07 08:00:19 -0500

    Add in API controllers

---
+++ app/Http/Controllers/Api/V1/EntriesController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use Auth;
+use App\Entries;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class EntriesController extends Controller
+{
+    public function index(Request $request)
+    {
+        $entries = Entries::where('user_id', auth()->user()->id)->orderBy('entry_time', 'desc')->simplePaginate(100);
+
+        // some fun code to split our data out by day
+        $days = [];
+        $this_day = $day_balance = null;
+
+        foreach($entries as $entry) {
+            $entry_time = Carbon::parse($entry->entry_time);
+            $day_text = $entry_time->format('D, j M');
+            $day_text = $entry_time->isToday() ? 'Today' : $day_text;
+            $day_text = $entry_time->isYesterday() ? 'Yesterday' : $day_text;
+
+            if(is_null($this_day)) $this_day = $day_text;
+            if($day_text != $this_day) {
+                $days[$this_day]['title'] = $this_day;
+                $days[$this_day]['balance'] = number_format($day_balance, 2);
+                $day_balance = 0;
+                $this_day = $day_text;
+            }
+            $day_balance += $entry->amount;
+            $days[$this_day]['entries'][$entry->id] = $entry->toArray();
+        }
+        $days[$this_day]['title'] = $this_day;
+        $days[$this_day]['balance'] = number_format($day_balance, 2);
+
+        $days['morePages'] = $entries->hasMorePages() ? 1 : 0;
+
+        return $days;
+    }
+
+    public function store(Request $request)
+    {
+        $entry = Entries::create($request->all());
+        return $entry;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $entry = Entries::findOrFail($id);
+        $entry->update($request->all());
+        return $entry;
+    }
+
+    public function destroy($id)
+    {
+        $entry = Entries::findOrFail($id);
+        $entry->delete();
+        return '';
+    }
+}
